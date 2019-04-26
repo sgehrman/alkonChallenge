fe874b154d39a88052b92f6c879ee9b54db55953
Steve Gehrman <sgehrman@gmail.com>
2019-04-25 19:31:52 -0700

    env

--- src/js/ore.js
+++ src/js/ore.js
@@ -1,21 +1,10 @@
-import dotenv from 'dotenv'
 import { OreId } from 'eos-auth'
 import scatterProvider from 'eos-transit-scatter-provider'
 import ledgerProvider from 'eos-transit-ledger-provider'
 import lynxProvider from 'eos-transit-lynx-provider'
 import meetoneProvider from 'eos-transit-meetone-provider'
 import tokenpocketProvider from 'eos-transit-tokenpocket-provider'
-
-dotenv.config()
-
-const {
-  REACT_APP_OREID_APP_ID: appId, // Provided when you register your app
-  REACT_APP_OREID_API_KEY: apiKey, // Provided when you register your app
-  REACT_APP_AUTH_CALLBACK: authCallbackUrl, // The url called by the server when login flow is finished - must match one of the callback strings listed in the App Registration
-  REACT_APP_SIGN_CALLBACK: signCallbackUrl, // The url called by the server when transaction signing flow is finished - must match one of the callback strings listed in the App Registration
-  REACT_APP_OREID_URL: oreIdUrl, // HTTPS Address of OREID server
-  REACT_APP_BACKGROUND_COLOR: backgroundColor, // Background color shown during login flow
-} = process.env
+import ENV from './env'
 
 export default class ORE {
   constructor() {
@@ -35,12 +24,12 @@ export default class ORE {
 
     this.id = new OreId({
       appName: 'ORE ID Sample App',
-      appId,
-      apiKey,
-      oreIdUrl,
-      authCallbackUrl,
-      signCallbackUrl,
-      backgroundColor,
+      appId: ENV.appId,
+      apiKey: ENV.apiKey,
+      oreIdUrl: ENV.oreIdUrl,
+      authCallbackUrl: ENV.authCallbackUrl,
+      signCallbackUrl: ENV.signCallbackUrl,
+      backgroundColor: ENV.backgroundColor,
       eosTransitWalletProviders,
       setBusyCallback: setBusyCallback,
     })
